518f09843b41b828a9048e34c9d159dd434e7f77
jzc <justincai.lps@gmail.com>
2018-12-25 17:05:11 -0700

    tests and scanner fixes

--- src/scanner.rs
+++ src/scanner.rs
@@ -15,14 +15,6 @@ pub struct ParseError {
     line: usize,
 }
 
-pub struct Scanner {
-    source: AsciiString,
-    start: usize,
-    current: usize,
-    line: usize,
-    tokens: Vec<Token>
-}
-
 fn is_whitespace(ch: u8) -> bool {
     (ch == b' ') || (ch == b'\n')
 }
@@ -31,9 +23,17 @@ fn is_numeric(ch: u8) -> bool {
     (ch >= b'0') && (ch <= b'9')
 }
 
-const missing_quote: &str = "Missing quote '\"'";
-const unexpected_quote: &str = "Unexpected quote '\"'";
-const open_paren_in_atom: &str = "Found illegal opening paren '(' in atom";
+const MISSING_QUOTE: &str = "Missing quote '\"'";
+const UNEXPECTED_QUOTE: &str = "Unexpected quote '\"'";
+const OPEN_PAREN_IN_ATOM: &str = "Found illegal opening paren '(' in atom";
+
+pub struct Scanner {
+    source: AsciiString,
+    start: usize,
+    current: usize,
+    line: usize,
+    tokens: Vec<Token>
+}
 
 impl Scanner {
     pub fn new<'a>(source: &'a [u8]) -> Self {
@@ -82,7 +82,8 @@ impl Scanner {
                     None => self.symbol(),
                     Some(_) => self.float(true),
                 },
-                b'"' => return self.parse_err(unexpected_quote),
+                b'"' => return self.parse_err(UNEXPECTED_QUOTE),
+                b'(' => return self.parse_err(OPEN_PAREN_IN_ATOM),
                 _ => return self.symbol(),
             }
         }
@@ -96,6 +97,7 @@ impl Scanner {
                 match self.advance().unwrap() {
                     ch if is_numeric(ch) => (),
                     b'e' | b'E' => { exponent_consumed = true; break; }
+                    b'(' => return self.parse_err(OPEN_PAREN_IN_ATOM),
                     _ => return self.symbol(),
                 }
             }
@@ -110,13 +112,17 @@ impl Scanner {
         }
         match self.peek() {
             None => (),
-            Some(ch) if (ch == b'+') || (ch == b'-') => { self.advance(); },
-            _ => return self.symbol(),
-        }
+            Some(ch) => match ch {
+                b'+' | b'-' => { self.advance(); },
+                ch if is_numeric(ch) => (),
+                _ => return self.symbol(),
+            }
+        };
 
         while self.is_more_token() {
             match self.advance().unwrap() {
                 ch if is_numeric(ch) => (),
+                b'(' => return self.parse_err(OPEN_PAREN_IN_ATOM),
                 _ => return self.symbol(),
             }
         }
@@ -128,10 +134,10 @@ impl Scanner {
     fn string(&mut self) -> Result<(), ParseError> {
         loop {
             match self.advance() {
-                None => return self.parse_err(missing_quote),
+                None => return self.parse_err(MISSING_QUOTE),
                 Some(ch) => match ch {
                     b'"' => { self.add_string_token(); return Ok(()); }
-                    b'\n' => return self.parse_err(missing_quote),
+                    b'\n' => return self.parse_err(MISSING_QUOTE),
                     _ => ()
                 }
             }
@@ -141,7 +147,7 @@ impl Scanner {
     fn symbol(&mut self) -> Result<(), ParseError> {
         while self.is_more_token() {
             match self.advance().unwrap() {
-                b'(' => return self.parse_err(open_paren_in_atom),
+                b'(' => return self.parse_err(OPEN_PAREN_IN_ATOM),
                 _ => ()
             }
         }
@@ -210,56 +216,194 @@ impl Scanner {
 mod tests {
     use super::*;
 
+    fn s(x: &'static str) -> Token { Token::Symbol(x.as_bytes().to_vec()) }
+    fn st(x: &'static str) -> Token { Token::String(x.as_bytes().to_vec()) }
+    fn i(x: i64) -> Token { Token::Int(x) }
+    fn f(x: f64) -> Token { Token::Float(x) }
+    fn op() -> Token { Token::OpenParen }
+    fn cp() -> Token { Token::ClosedParen }
+    fn tokens(x: &'static str) -> Result<Vec<Token>, ParseError> { 
+        let xstr = x.as_bytes();
+        let mut scanner = Scanner::new(xstr);
+        scanner.scan_tokens()
+    }
+
     #[test]
     fn test_scan_tokens() {
-        let mut scanner;
         let mut res;
-        let s = |x: &[u8]| Token::Symbol(x.to_vec()); 
-        let i = |x: i64| Token::Int(x);
 
-        scanner = Scanner::new(b"()");
-        res = scanner.scan_tokens();
+        res = tokens("()");
         assert!(res.is_ok());
-        assert_eq!(res.ok().unwrap(), [Token::OpenParen, Token::ClosedParen]);
+        assert_eq!(res.ok().unwrap(), [op(), cp()]);
         
-        scanner = Scanner::new(b"(ab( 1 2)");
-        res = scanner.scan_tokens();
+        res = tokens("(ab( 1 2)");
         assert!(res.is_err());
         assert_eq!(res.err().unwrap().line, 1);
 
-        scanner = Scanner::new(b"(1 2\nab( 3)");
-        res = scanner.scan_tokens();
+        res = tokens("(1 2\nab( 3)");
         assert!(res.is_err());
         assert_eq!(res.err().unwrap().line, 2);
 
-        let sources: Vec<&[u8]> = vec![
-            b"(+ abc def)",
-            b"( + abc def)",
-            b"(+ abc def )",
-            b"(+  abc    def)",
-            b"    (+ abc def)  ",
-            b"(+\nabc\ndef)",
+        let sources = vec![
+            "(+ abc def)",
+            "( + abc def)",
+            "(+ abc def )",
+            "(+  abc    def)",
+            "    (+ abc def)  ",
+            "(+\nabc\ndef)",
         ];
         for si in sources {
-            scanner = Scanner::new(si);
-            res = scanner.scan_tokens();
+            res = tokens(si);
             assert!(res.is_ok());
-            assert_eq!(res.ok().unwrap(), [Token::OpenParen, s(b"+"), s(b"abc"), s(b"def"), Token::ClosedParen]);
+            assert_eq!(res.ok().unwrap(), [op(), s("+"), s("abc"), s("def"), cp()]);
         }
 
-        scanner = Scanner::new(b"(() (1 2 3))");
-        res = scanner.scan_tokens();
+
+        res = tokens("(() (1 2 3))");
         assert!(res.is_ok());
         assert_eq!(res.ok().unwrap(), [Token::OpenParen, 
             Token::OpenParen, Token::ClosedParen,
             Token::OpenParen, i(1), i(2), i(3), Token::ClosedParen,
             Token::ClosedParen]);
 
-        scanner = Scanner::new(b"(#$%-a)");
-        res = scanner.scan_tokens();
+        res = tokens("(#$%-a)");
         assert!(res.is_ok());
-        assert_eq!(res.ok().unwrap(), [Token::OpenParen, s(b"#$%-a"), Token::ClosedParen])
+        assert_eq!(res.ok().unwrap(), [op(), s("#$%-a"), cp()])
+    }
+
+    #[test]
+    fn test_scan_int() {
+        let mut res;
+
+        res = tokens("123");
+        assert!(res.is_ok());
+        assert_eq!(res.ok().unwrap(), [i(123)]);
+
+        res = tokens("(1");
+        assert!(res.is_ok());
+        assert_eq!(res.ok().unwrap(), [op(), i(1)]);
+
+        let sources = vec![
+            " 123",
+            "0123",
+            " 0123",
+            " +0123",
+            "+0123",
+        ];
+        for si in sources {
+            res = tokens(si);
+            assert!(res.is_ok());
+            assert_eq!(res.ok().unwrap(), [i(123)]);
+        }
+
+
+        res = tokens(" +01123(3");
+        assert!(res.is_err());
+
+        res = tokens("(+011233)");
+        assert!(res.is_ok());
+        assert_eq!(res.ok().unwrap(), [op(), i(11233), cp()]);
+    }
+
+    #[test]
+    fn test_scan_string() {
+        let mut res;
+
+        res = tokens(r#" ("abc") "#);
+        assert!(res.is_ok());
+        assert_eq!(res.ok().unwrap(), [op(), st("abc"), cp()]);
+
+        res = tokens(r#" "(abc))())(" "#);
+        assert!(res.is_ok());
+        assert_eq!(res.ok().unwrap(), [st("(abc))())(")]);
+
+        res = tokens(" \"a\nb\"  ");
+        assert!(res.is_err());
+
+        res = tokens("(\")");
+        assert!(res.is_err());
+
+        res = tokens(r#"  ("abc" "def" ("ijk")) "#);
+        assert!(res.is_ok());
+        assert_eq!(res.ok().unwrap(), [op(), st("abc"), st("def"), op(), st("ijk"), cp(), cp()]);
+    }
+
+    #[test]
+    fn test_scan_float() {
+        let mut res;
+        let mut sources;
+
+        sources = vec![
+            "1.0",
+            "01.0",
+            "+1.0",
+            "+01.0",
+            "+01.",
+            "01.",
+            "1.",
+            "1.00",
+            "+1.00",
+            "+01.00",
+        ];
+        for si in sources {
+            res = tokens(si);
+            assert!(res.is_ok());
+            assert_eq!(res.ok().unwrap(), [f(1.0)]);
+        }
+
+        sources = vec![
+            "1e1",
+            "+1e1",
+            "+1e+1",
+            "1e+01",
+            "001e01",
+            "001.0e+01",
+            "1.e+1",
+            "1.e+001",
+            "001.e01",
+        ];
         
+        for si in sources {
+            res = tokens(si);
+            assert!(res.is_ok());
+            assert_eq!(res.ok().unwrap(), [f(1e1)]);
+        }
+
+        sources = vec![
+            "1(e1",
+            "1e1(",
+            "1.e1(",
+            "1.e(1",
+        ];
+
+        for si in sources {
+            res = tokens(si);
+            assert!(res.is_err());
+        }
+
+        res = tokens("1.1.");
+        assert!(res.is_ok());
+        assert_eq!(res.ok().unwrap(), [s("1.1.")]);
+
+        res = tokens("1.e15.");
+        assert!(res.is_ok());
+        assert_eq!(res.ok().unwrap(), [s("1.e15.")]);
+
+        res = tokens("1.e");
+        assert!(res.is_ok());
+        assert_eq!(res.ok().unwrap(), [s("1.e")]);
+
+        res = tokens(".1");
+        assert!(res.is_ok());
+        assert_eq!(res.ok().unwrap(), [f(0.1)]);
+
+        res = tokens(".");
+        assert!(res.is_ok());
+        assert_eq!(res.ok().unwrap(), [s(".")]);
+
+        res = tokens("3.14156e-03");
+        assert!(res.is_ok());
+        assert_eq!(res.ok().unwrap(), [f(3.14156e-03)]);
     }
 
     #[test]
